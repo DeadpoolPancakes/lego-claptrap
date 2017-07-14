4ad96f3ac685b1a61746cf39180a67f4fbae7b67
Jamie Naylor <jamie@naylorwebsites.co.uk>
2017-07-14 11:01:18 +0100

    added basic function
    basic program to test motor connection and library imports etc

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import sys
 import time
 import piconzero
+
+
+piconzero.forward(50)
+time.sleep(3)
+piconzero.stop()
+time.sleep(1)
+piconzero.spinleft(50)
+time.sleep(1)
+piconzero.spinright(50)
+time.sleep(1)
+piconzero.revers(50)
+time.slee(3)
+piconzero.stop()
