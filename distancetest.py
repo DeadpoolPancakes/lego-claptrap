61d31056e421effa004047d5dc26e53c8ccb885f
Jamie Naylor <drnailher@gmail.com>
2017-07-22 19:57:00 +0100

    initial program
    this one actually compiles and works!

--- distancetest.py
+++ distancetest.py
@@ -5,21 +5,32 @@ import hcsr04, time
 
 hcsr04.init()
 pz.init()
-distance = int(hcsr04.getDistance())
-while distance > 10:
-    pz.forward(50)
-else    
-    while distance < 10
-        pz.backward(10)
-        if distance > 10
+
+try:
+    while True:
+        distance = int(hcsr04.getDistance())
+        while distance > 25:
+            pz.setMotor(0,-100)
+            pz.setMotor(1, 100)
+            #pz.forward(100)
+            distance = int(hcsr04.getDistance())
+            print "Distance:", distance
+            print "weeeeeeeee"
+            time.sleep(0.1)
+        else:
+            print "uhoh"
             pz.stop()
-            sleep.time(1)
-            pz.spinLeft(50)
-            time.sleep(2)
-            pz.stop() 
-
+            time.sleep(1)
+            print "backing up"
+            pz.setMotor(0,100)
+            pz.setMotor(1,-100)
+            time.sleep(1)
+            print "looking for new rout"
+            pz.setMotor(0,-100)
+            pz.setMotor(1,-100)
+            time.sleep(1)
 except KeyboardInterrupt:
-
+    print
 finally:
     hcsr04.cleanup()
     pz.cleanup()
