ead4175ff03ec007b26976bad97f31b432ff16bd
Jamie Naylor <jamie@naylorwebsites.co.uk>
2017-08-29 09:35:54 +0100

    added debugging paramater
    added the option to set the print lines on or off

--- distancetest.py
+++ distancetest.py
@@ -7,6 +7,8 @@ import hcsr04, time
 hcsr04.init()
 pz.init()
 
+debug = True
+
 try:
     while True:
         distance = int(hcsr04.getDistance())
@@ -15,8 +17,9 @@ try:
             pz.setMotor(0,-100)
             pz.setMotor(1, 100)
             distance = int(hcsr04.getDistance())
-            print ("Distance:", distance)
-            print ("weeeeeeeee")
+            if debug = true:
+                print ("Distance:", distance)
+                print ("weeeeeeeee")
             time.sleep(0.1)
         else:
             print ("uhoh")
@@ -24,7 +27,8 @@ try:
             pz.stop()
             rdistance = int(sd.getDistance())
             time.sleep(0.5)
-            print ("backing up")
+            if debug = true:
+                print ("backing up")
             #back up
             while rdistance > 20 or i < 5: 
                 rdistance = int(sd.getDistance())
@@ -32,29 +36,34 @@ try:
                 pz.setMotor(1,-100)
                 i = i + 1
                 time.sleep(0.1)
-            else    
-            print ("looking for new route")
+            else
+            if debug = true:    
+                print ("looking for new route")
             i = 0
             #rotate left and check distance
             pz.setMotor(0,-100)
             pz.setMotor(1, -100)
             time.sleep(0.5)
             distanceleft = int(hcsr04.getDistance())
-            print ("Distance left:", distanceleft)
+            if debug = true:
+                print ("Distance left:", distanceleft)
             #rotate right and check distance
             pz.setMotor(0,100)
             pz.setMotor(1, 100)
             time.sleep(1)
             distanceright = int(hcsr04.getDistance())
-            print ("Distance right:", distanceright)
+            if debug = true:
+                print ("Distance right:", distanceright)
             if distanceright < distanceleft:
-                print ("going left")
+                if debug = true:
+                    print ("going left")
                 pz.setMotor(0,-100)
                 pz.setMotor(1, -100)
                 time.sleep(1)
                 distance = int(hcsr04.getDistance())
             else:
-                print ("going right")
+                if debug = true:
+                    print ("going right")
                 distance = int(hcsr04.getDistance())
 
 except KeyboardInterrupt:
