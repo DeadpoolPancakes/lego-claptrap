1095bcc19788d184a02ddb090071f30a4c2ed1b5
Jamie Naylor <drnailher@gmail.com>
2017-09-03 18:50:04 +0100

    got rear sensor working
    had to init the secondistance code like a noob

--- distancetest.py
+++ distancetest.py
@@ -6,12 +6,13 @@ import hcsr04, time
 
 hcsr04.init()
 pz.init()
+sd.init()
 
 try:
     while True:
         distance = int(hcsr04.getDistance())
         rdistance = int(sd.getDistance())
-        while distance > 20:
+        while distance > 30:
             pz.setMotor(0,-100)
             pz.setMotor(1, 100)
             distance = int(hcsr04.getDistance())
@@ -26,36 +27,38 @@ try:
             time.sleep(0.5)
             print ("backing up")
             #back up
-            while rdistance > 20 or i < 5: 
+            while rdistance > 30 and i < 5: 
                 rdistance = int(sd.getDistance())
+                print ("rear distance", rdistance);
+                print i
                 pz.setMotor(0,100)
                 pz.setMotor(1,-100)
                 i = i + 1
                 time.sleep(0.1)
-            else    
-            print ("looking for new route")
-            i = 0
-            #rotate left and check distance
-            pz.setMotor(0,-100)
-            pz.setMotor(1, -100)
-            time.sleep(0.5)
-            distanceleft = int(hcsr04.getDistance())
-            print ("Distance left:", distanceleft)
-            #rotate right and check distance
-            pz.setMotor(0,100)
-            pz.setMotor(1, 100)
-            time.sleep(1)
-            distanceright = int(hcsr04.getDistance())
-            print ("Distance right:", distanceright)
-            if distanceright < distanceleft:
-                print ("going left")
+            else:    
+                print ("looking for new route")
+                i = 0
+                #rotate left and check distance
                 pz.setMotor(0,-100)
                 pz.setMotor(1, -100)
                 time.sleep(1)
-                distance = int(hcsr04.getDistance())
-            else:
-                print ("going right")
-                distance = int(hcsr04.getDistance())
+                distanceleft = int(hcsr04.getDistance())
+                print ("Distance left:", distanceleft)
+                #rotate right and check distance
+                pz.setMotor(0,100)
+                pz.setMotor(1, 100)
+                time.sleep(2)
+                distanceright = int(hcsr04.getDistance())
+                print ("Distance right:", distanceright)
+                if distanceright < distanceleft:
+                    print ("going left")
+                    pz.setMotor(0,-100)
+                    pz.setMotor(1, -100)
+                    time.sleep(2)
+                    distance = int(hcsr04.getDistance())
+                else:
+                    print ("going right")
+                    distance = int(hcsr04.getDistance())
 
 except KeyboardInterrupt:
     print ("exiting")
