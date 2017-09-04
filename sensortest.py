bebdf3b08e21c73b28bf9ac60c69006c8a1bd995
Jamie Naylor <jamie@naylorwebsites.co.uk>
2017-09-04 10:59:41 +0100

    added a test for the new function

---
+++ sensortest.py
@@ -0,0 +1,20 @@
+import sys
+import time
+import distanceSensor as ds
+
+ds.init()
+
+
+try:    
+    while True:
+        distanceone = int(ds.getDistance(12,15))
+        distancetwo = int(ds.getDistance(38, 38))
+        print ("Distance One:", distanceone)
+        print ("Distance Two:", distancetwo)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print ("exiting")
+finally:
+    secondistance.cleanup()
+      
