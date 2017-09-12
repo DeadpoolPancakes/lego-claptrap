454e755650c92840b9eb6cf55d5ee40e4cce8b94
Jamie Naylor <drnailher@gmail.com>
2017-09-12 20:47:35 +0100

    first attempt at solving the corner problem

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ sd.init()
 
 filename = "log.txt"
 file = open(filename,"r+")
-
+left = 0
+right = 0
 
 try:
     while True:
@@ -54,18 +55,30 @@ try:
                 time.sleep(2)
                 distanceright = int(hcsr04.getDistance())
                 print ("Distance right:", distanceright)
-                if distanceright < distanceleft:
+                if distanceright <= distanceleft:
                     print ("going left")
-                    file.write("left")
+                    file.write("left /n")
                     pz.setMotor(0,-100)
                     pz.setMotor(1, -100)
                     time.sleep(2)
                     distance = int(hcsr04.getDistance())
+                    left = left + 1
+                elif left > 0:
+                    print ("going left")
+                    pz.setMotor(0,-100)
+                    pz.setMotor(1, -100)
+                    time.sleep(2)
+                    file.write("left /n")
+                    distance = int(hcsr04.getDistance())
+                    right = right + 1
                 else:
                     print ("going right")
-                    file.write("right")
+                    pz.setMotor(1,-100)
+                    pz.setMotor(0, -100)
+                    time.sleep(2)
+                    file.write("right /n")
                     distance = int(hcsr04.getDistance())
-
+                    right = right + 1
 except KeyboardInterrupt:
     print ("exiting")
 finally:
