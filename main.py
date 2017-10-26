c474ce6bfb382ab338a29272542b257a5d74b6b7
Jamie <jamie.naylor@dezrez.com>
2017-10-26 11:21:42 +0100

    Updated main program to utilise new chasis

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ try:
         rdistance = int(sd.getDistance())
         while distance > 25:
             f = f + 1
-            pz.setMotor(0,-100)
-            pz.setMotor(1, 100)
+            pz.forward(50)
             distance = int(hcsr04.getDistance())
             if f == 100:
                 print ("Distance:", distance)
@@ -42,41 +41,36 @@ try:
             while rdistance > 30 and i < 50: 
                 rdistance = int(sd.getDistance())
                 file.write("backward\n")
-                pz.setMotor(0,100)
-                pz.setMotor(1,-100)
+                pz.backward(50)
                 i = i + 1
                 time.sleep(0.01)
             else:    
                 print ("looking for new route")
                 i = 0
                 #rotate left and check distance
-                pz.setMotor(0,-100)
-                pz.setMotor(1, -100)
-                time.sleep(1)
+                pz.spinLeft(50)
+                time.sleep(0.5)
                 distanceleft = int(hcsr04.getDistance())
                 time.sleep(0.1)
                 print ("Distance left:", distanceleft)
                 #rotate right and check distance
-                pz.setMotor(0,100)
-                pz.setMotor(1, 100)
-                time.sleep(2)
+                pz.spinRight(50)
+                time.sleep(1)
                 distanceright = int(hcsr04.getDistance())
                 time.sleep(0.1)
                 print ("Distance right:", distanceright)
                 # if stuck in a loop of left right actions this should break it out of it
                 if left >= 5 and right >= 5:
                     print ("getting out of corner")
-                    pz.setMotor(0,-100)
-                    pz.setMotor(1, -100)
-                    time.sleep(5)
+                    pz.spinLeft(50)
+                    time.sleep(2)
                     right = 0
                     left = 0
                 elif distanceright <= distanceleft:
                     print ("going left")
                     file.write("left\n")
-                    pz.setMotor(0,-100)
-                    pz.setMotor(1, -100)
-                    time.sleep(2)
+                    pz.spinLeft(50)
+                    time.sleep(1)
                     distance = int(hcsr04.getDistance())
                     left = left + 1
                 else:
